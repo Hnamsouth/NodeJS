794c814982373b99efe44822bd8a5e2341c94749
hoang nam <namhvth2204008@fpt.edu.vn>
2022-08-25 11:12:02 +0700

    1

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,15 +1,37 @@
 //const express = require('express')
 import express from 'express'
+import mysql from 'mysql2'
 
 
 const port = process.env.PORT || 4567
-
 const app = express();
 
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    database: "test"
+})
+
+// create
 app.listen(port, () => {
     console.log(`server running....${port}`)
 })
-
 app.get("/", (req, res) => {
     res.send("hello nodejs")
+})
+
+var qr1 = 'select * from class join students on students.ClassID= class.id'
+
+con.query(qr1, (err, data, feilds) => {
+    app.get("/api1", (req, res) => {
+            if (err) res.send('Error..!!')
+            res.json(data)
+        })
+        //console.table(data)
+        //console.log(data)
+})
+app.get("/api2", (req, res) => {
+    con.query(`select * from students`, (err, data) => {
+        res.send(data)
+    })
 })
